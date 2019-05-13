f5e143ecf915d647550af68543cab96e4410cc8e
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2019-05-13 17:44:21 +0200

    Initial functional withError HoC

---
+++ next-with-error.tsx
@@ -0,0 +1,57 @@
+/**
+ * This higher-order component is used to render the Error page if a HTTP status
+ * code >400 is thrown by one of the pages.
+ *
+ * To trigger it, just add statusCode to the object returned by getInitialProps
+ *
+ * This solution is based on Tim Neutkens's insights and inspired by Nuxt.js
+ * https://spectrum.chat/next-js/general/error-handling-in-async-getinitialprops~99400c6c-0da8-4de5-aecd-2ecf122e8ad0
+ * https://github.com/nuxt/nuxt.js/issues/895#issuecomment-308682972
+ */
+
+import React from 'react';
+import ErrorPage from 'next/error';
+import { default as NextApp, NextAppContext, AppProps, DefaultAppIProps } from 'next/app';
+
+interface WithErrorProps {
+  error?: {
+    statusCode: number;
+  };
+}
+
+interface AppInitialProps {
+  pageProps: WithErrorProps;
+}
+
+const withError = function<P extends WithErrorProps>(WrappedComponent: typeof NextApp) {
+  return class WithError extends React.Component<P & WithErrorProps & AppProps & DefaultAppIProps> {
+    public static getInitialProps = async (appContext: NextAppContext) => {
+      let appProps: AppInitialProps = { pageProps: {} };
+
+      if (WrappedComponent.getInitialProps) {
+        appProps = await WrappedComponent.getInitialProps(appContext);
+      }
+
+      const { res } = appContext.ctx;
+      const { error } = appProps.pageProps;
+
+      if (error && res) {
+        res.statusCode = error.statusCode;
+      }
+
+      return appProps;
+    };
+
+    render() {
+      const { error } = this.props.pageProps;
+
+      if (error && error.statusCode >= 400) {
+        return <ErrorPage statusCode={error.statusCode} />;
+      }
+
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+};
+
+export default withError;
